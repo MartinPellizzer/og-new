f22d8925ac71e26cc22b729213ab08399da1f9fe
Martin <martinpellizzer@gmail.com>
2025-01-10 12:18:59 +0100

    first commit

---
+++ website/main.py
@@ -0,0 +1,122 @@
+sections = []
+section = f'''
+    <section>
+        <div class="container-xl">
+            <h1>Il vecchio metodo (sanificazione tradizionale)</h1>
+        </div>
+    </section>
+'''
+sections.append(section)
+section = f'''
+    <section>
+        <div class="container-xl">
+            <div class="flex gap-48">
+                <div class="flex-1">
+                    <h1>3 - INGREDIENTI</h1>
+                    <p>Ingredienti come farina, frutta, verdura, carne, latte, formaggi, spezie e molti altri (persino l'acqua), vengono contaminati facilmente da batteri, virus e parassiti. Questo accade durante la coltivazione, la raccolta, il trasporto, la manipolazione e le altre fasi produttive.</p>
+                    <p>Di conseguenza, la sanificazione degli ingredienti è fondamentale per evitare malattie e rispettare le normative di sicurezza alimentare.</p>
+                    <p>Per sanificare gli ingredienti con i sistemi tradizionali, e necessario:</p>
+                    <ul>
+                        <li><strong>Scegliere acqua di alta qualità</strong>: se l'acqua ha dei contaminati, questi vengono trasferiti agli ingredienti che diventano pericolosi per la salute</li>
+                        <li><strong>Lavare accuratamente con acqua</strong>: prima di usare disinfettanti chimici bisogna rimuovere sporco, terra e detriti visibili dalle materie prime con acqua pulita</li>
+                        <li><strong>Scegliere trattamenti antimicrobici</strong>: dopo il lavaggio, vanno scelti i disinfettanti più adatti al tipo di contaminazione e materia prima (come cloro, perossido di idrogeno o altri)</li>
+                        <li><strong>Verificare la concentrazione dei disinfettanti</strong>: per usare i disinfettanti, bisogna assicurarsi che le soluzioni antimicrobiche siano alla giusta concentrazione per essere efficaci senza lasciare residui chimici tossici nelle materie prime</li>
+                        <li><strong>Eseguire il risciacquo finale</strong>: dopo il trattamento con antimicrobici, vanno risciacquati gli ingredienti per rimuovere quanti più residui chimici possibili</li>
+                        <li><strong>Usare sistemi supplementari</strong>: se i prodotti chimici non bastano, si utilizzano sistemi supplementari come trattamenti termici (forni, vapori, ecc…) o UV</li>
+                    </ul>
+                    <p>Questo sistema tradizionale di sanificazione alle volte funziona, alle volte non funziona.</p>
+                    <p>Infatti, spesso è difficile capire quale agente antimicrobico è quello giusto, quale sia la concentrazione giusta e qual è il tempo di contatto giusto.</p>
+                    <p>Per di più, è facile danneggiare le materie prime, il risultato è difficile da garantire e vengono sprecate molte risorse (come l'acqua di risciacquo, i tempi lunghi di sanificazione e il costo energetico dei sistemi termici).</p>
+                    <p>Vediamo quindi come risolvere tutti questi problemi, sanificando tutti i tipi di patogeni con un unico sistema che funziona velocemente (riducendo i tempi di inattività), non lascia residui chimici (quindi non serve fare il risciacquo finale) e consuma pochissima energia elettrica (non pesa sulla bolletta).</p>
+                </div>
+                <div class="flex-1">
+                    <img src="/test-ingredients.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+'''
+sections.append(section)
+section = f'''
+    <section>
+        <div class="container-xl">
+            <h1>Il nuovo metodo (sanificazione ozono)</h1>
+La sanificazione ad ozono è un metodo efficace usato nell'industria alimentare per eliminare batteri, virus, muffe, parassiti, odori e altri contaminanti da superfici, ambienti chiusi (aria) e ingredienti.
+Mantiene la sicurezza alimentare, prolunga la durata di conservazione e previene contaminazioni senza lasciare residui chimici dannosi, in modo da soddisfare gli standard normativi.
+I vantaggi dell'ozono sono:
+Veloce: L'ozono attacca i microbi molto velocemente (secondi/minuti) e riduce i tempi di inattività delle operazioni di sanificazione
+Versatile: L'ozono è antibatterico, antivirale e antifungino, quindi può essere usato come soluzione unica per una disinfezione completa (evitando di dover usare disinfettanti diversi per applicazioni diverse) 
+Semplice: L'ozono non richiede complicate procedure di preparazione, applicazione e smaltimento (non serve avere una laurea in chimica per usarlo).
+Efficace: L'ozono elimina i patogeni già a basse concentrazioni (2 ppm) dato il suo elevato potere ossidante (maggiore del cloro e degli altri disinfettanti) 
+Ecologico: L'ozono non lascia residui chimici ed è sicuro per l'ambiente, dato che si riconverte completamente in ossigeno dopo il trattamento di sanificazione
+Efficiente: L'ozono richiede solo una minima quantità di elettricità per essere prodotto e funziona anche in ambienti molto freddi (con notevoli risparmi sul riscaldamento)
+Per sanificare con l'ozono sono necessari solo 3 passaggi:
+Generazione: L'ozono viene prodotto utilizzando generatori di ozono, i quali convertono le molecole di ossigeno (O2) in ozono (O3)
+Applicazione: L'ozono viene applicato direttamente sulle superfici, nell'aria degli ambienti chiusi o sugli ingredienti (in forma gassosa o acquosa)
+Monitoraggio: L'ozono viene monitorato per confermare che la concentrazione e il tempo di esposizione siano giusti (con sensori e allarmi)
+Qui sotto trovi una procedura completa per sanificare con l'ozono. 
+        </div>
+    </section>
+'''
+sections.append(section)
+section = f'''
+    <section>
+        <div class="container-xl">
+            <h1>Come sanificare con l'ozono (in poche parole)</h1>
+Generazione dell'ozono
+Passo 1: Installa un generatore di ozono progettato per applicazioni nell'industria alimentare, assicurandoti che produca ozono pulito e che rispetti gli standard di sicurezza del settore
+Passo 2: Imposta il generatore di ozono per produrre la concentrazione richiesta di ozono, di solito in parti per milione (ppm)
+Passo 3: Continua a produrre ozono per fare in modo che resti a contatto con il patogeno da eliminare il tempo necessario
+Applicazione dell'ozono
+Passo 1: Per disinfettare materie prime e superfici di contatto degli alimenti, applica l'ozono in forma acquosa o gassosa direttamente sulla parte da disinfettare 
+Passo 2: Per disinfettare l'aria degli ambienti, immetti il gas ozono dentro l'ambiente da trattare (sigillato il più possibile) e assicurati che la ventilazione sia adeguata
+Passo 3: Per ridurre la possibilità che si sviluppino future contaminazioni, applica regolarmente piccole quantità di gas ozono negli ambienti (a scopo di prevenzione)
+Monitoraggio dell'ozono
+Passo 1: Controlla regolarmente i livelli di ozono con sensori per mantenere concentrazioni ottimali
+Passo 2: Utilizza allarmi e sistemi di spegnimento automatico per prevenire esposizioni eccessive all'ozono
+Passo 3: Esegui regolarmente la manutenzione del generatore di ozono per garantire l'efficienza e il corretto funzionamento
+Se segui tutti i passi della precedente procedura, ottieni una sanificazione impeccabile.
+In tutta onestà, però, possiamo garantirti che sono poche le aziende alimentari che sono in grado di seguire questi passi in modo corretto. 
+Infatti, capire quale generatore usare, a quale concentrazione usarlo, per quanto tempo usarlo, ogni quanto usarlo, come usarlo, come controllarlo e come manutenzionarlo, richiede strumenti all'avanguardia e competenze molto specifiche (che poche aziende hanno).
+Quindi, per rendere tutto questo possibile (e semplice), ti presentiamo il nuovo sistema di sanificazione ad ozono di Ozonogroup.
+        </div>
+    </section>
+'''
+sections.append(section)
+section = f'''
+    <section>
+        <div class="container-xl">
+            <h1>Il Nuovo Sistema di Sanificazione di Ozonogroup</h1>
+Ecco i benefici nell'usare il nuovo sistema di sanificazione ad ozono di Ozonogroup rispetto ad altri sistemi di sanificazione ad ozono:
+Elevato potere disinfettante: Il sistema di sanificazione Ozonogroup distrugge batteri, virus, muffe, lieviti e altri patogeni tipici dell'industria alimentare (inclusi E. coli, Salmonella, Listeria e altri patogeni resistenti). Inoltre, rimuove con facilità gli odori sgradevoli derivanti da contaminazioni o processi produttivi e riduce pesticidi, residui chimici e contaminanti organici.
+Applicazioni multiple: Il sistema Ozonogroup è utilizzabile in vari ambiti dell'industria alimentare, inclusi impianti di lavorazione, stoccaggio, trasporto e confezionamento. Questo perché è possibile regolare i livelli di ozono in base alla dimensione dell'ambiente e al tipo di contaminante.
+Automatizzato: Il sistema Ozonogroup dà la possibilità di scegliere gli orari della sanificazione tramite un calendario impostabile dai tecnici. Dopo che il sistema viene configurato la prima volta, lavora in autonomia senza continuo intervento da parte degli operatori.
+Sicuro: Il sistema Ozonogroup usa sensori per rilevare la concentrazione di ozono, in modo da evitare esposizioni pericolose per il personale. In aggiunta, ha un sistema di allarme che avvisa in caso di concentrazioni superiori ai limiti di sicurezza e funzioni di spegnimento se i livelli di ozono sono troppo alti o se il sistema rileva malfunzionamenti.
+Ozono pulito: Il sistema Ozonogroup produce ozono puro, ovvero non contaminato da altre sostanze che possono finire nei prodotti alimentari. Al contrario, la produzione di ozono tramite sistemi convenzionali genera dei sottoprodotti nocivi, quindi questi sistemi non sono idonei ad essere usati nell'industria alimentare.
+Risparmio energetico: Il sistema Ozonogroup usa una tecnologia di produzione del gas ozono che fa risparmiare molto sulla bolletta. Infatti, richiede solo una minima quantità di energia elettrica per produrre notevoli quantità di ozono e sanificare anche ambienti di grandi dimensioni. Inoltre, completa i cicli di sanificazione in tempi brevi, aumentando ulteriormente l'efficienza del processo produttivo.
+Installazione e manutenzione facile: Il sistema Ozonogroup è facilmente integrabile nell'infrastruttura esistente e usa componenti durevoli e facili da revisionare, riducendo al minimo il rischio di guasti. In aggiunta, i componenti sono di facile accesso per i tecnici, il che consente una facile pulizia e sostituzione se necessario.
+        </div>
+    </section>
+'''
+sections.append(section)
+sections_html = ''.join(sections)
+
+html = f'''
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.css">
+    <title>Ozonogroup | Sanificazione Ozono</title>
+</head>
+
+<body>
+    {sections_html}
+</body>
+
+</html>
+'''
+
+with open('index.html', 'w', encoding='utf-8') as f: f.write(html)
