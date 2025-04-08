a78116698d579b0847d021b21ea3b39e934b5e0d
Martin <martinpellizzer@gmail.com>
2025-04-08 08:33:50 +0200

    '

--- products/ozonanopro/arduino/__main__/__main__.ino
+++ products/ozonanopro/arduino/__main__/__main__.ino
@@ -58,6 +58,13 @@ typedef struct pressure_switch_t {
 } pressure_switch_t;
 pressure_switch_t pressure_switch = {};
 
+typedef struct valve_input_t {
+  int8_t state_old = -1;
+  int8_t state_cur = 0;
+  int8_t nextion_refresh = 0;
+} valve_input_t;
+valve_input_t valve_input = {};
+
 typedef struct valve_bypass_t {
   int8_t state_old = -1;
   int8_t state_cur = 0;
@@ -94,7 +101,14 @@ typedef struct ozone_generator_t {
 } ozone_generator_t;
 ozone_generator_t ozone_generator = {};
 
-#define PUMP_VALVE_GPIO 5
+typedef struct solenoid_t {  
+  int8_t state_old = -1;
+  int8_t state_cur = 0;
+  int8_t nextion_refresh = 0;
+} solenoid_t;
+solenoid_t solenoid = {};
+
+#define VALVE_INPUT_GPIO 5
 #define PUMP_BOOSTER_GPIO 18
 #define PUMP_NANO_GPIO 19
 #define FLOW_SWITCH_GPIO 25
@@ -104,10 +118,31 @@ ozone_generator_t ozone_generator = {};
 
 #define OXYGEN_CONCENTRATOR_GPIO 32
 #define OZONE_GENERATOR_GPIO 33
+#define SOLENOID_GPIO 23
 
-uint16_t cycle_state = 0;
+typedef struct cycle_t {  
+  uint16_t state_old = -1;
+  uint16_t state_cur = 0;
+  uint16_t pressure_switch_state_old = -1;
+  uint16_t pressure_switch_state_cur = 0;
+  uint16_t flow_switch_state_old = -1;
+  uint16_t flow_switch_state_cur = 0;
+} cycle_t;
+cycle_t cycle = {};
+
 uint32_t cycle_millis = 0;
 
+void valve_input_state_update(uint8_t val)
+{
+  valve_input.state_cur = val;
+  if (valve_input.state_old != valve_input.state_cur) 
+  {
+    valve_input.state_old = valve_input.state_cur;
+    valve_input.nextion_refresh = 1;
+    if (valve_input.state_cur == 1) { digitalWrite(VALVE_INPUT_GPIO, HIGH); }
+    else { digitalWrite(VALVE_INPUT_GPIO, LOW); }
+  }
+}
 
 void valve_bypass_1_state_update(uint8_t val)
 {
@@ -140,21 +175,48 @@ void pressure_switch_state_update()
   {
     pressure_switch.state_old = pressure_switch.state_cur;
     pressure_switch.nextion_refresh = 1;
+    cycle.pressure_switch_state_cur = pressure_switch.state_cur;
+  }
+}
+
+/*
+void pressure_switch_state_update()
+{
+  pressure_switch.state_cur = !digitalRead(PRESSURE_SWITCH_GPIO);
+  if (pressure_switch.state_old != pressure_switch.state_cur) 
+  {
+    pressure_switch.state_old = pressure_switch.state_cur;
+    pressure_switch.nextion_refresh = 1;
     if (pressure_switch.state_cur == 1)
     {
-      valve_bypass_1_state_update(1);
-      valve_bypass_2_state_update(0);
-      cycle_state = 100;
-    }
-    else
-    {
+      valve_input_state_update(0);
       valve_bypass_1_state_update(0);
       valve_bypass_2_state_update(1);
       oxygen_concentrator_state_update(0);
       ozone_generator_state_update(0);
-      cycle_state = 0;
+      solenoid_state_update(0);
+      cycle.state_cur = 0;
+    }
+    else
+    {
+      valve_input_state_update(1);
+      valve_bypass_1_state_update(1);
+      valve_bypass_2_state_update(0);
+      cycle.state_cur = 100;
     }
     cycle_millis = millis();
+  }
+}
+*/
+
+void flow_switch_state_update()
+{
+  flow_switch.state_cur = !digitalRead(FLOW_SWITCH_GPIO);
+  if (flow_switch.state_old != flow_switch.state_cur) 
+  {
+    flow_switch.state_old = flow_switch.state_cur;
+    flow_switch.nextion_refresh = 1;
+    cycle.flow_switch_state_cur = flow_switch.state_cur;
   }
 }
 
@@ -164,15 +226,21 @@ void pump_booster_state_update(uint8_t val)
   if (pump_booster.state_old != pump_booster.state_cur) 
   {
     pump_booster.state_old = pump_booster.state_cur;
-    if (pump_booster.state_cur == 1) 
-    {
-      digitalWrite(PUMP_BOOSTER_GPIO, HIGH);
-    }
-    else
-    {
-      digitalWrite(PUMP_BOOSTER_GPIO, LOW);
-    }
     pump_booster.nextion_refresh = 1;
+    if (pump_booster.state_cur == 1) { digitalWrite(PUMP_BOOSTER_GPIO, HIGH); }
+    else { digitalWrite(PUMP_BOOSTER_GPIO, LOW); }
+  }
+}
+
+void pump_nano_state_update(uint8_t val)
+{
+  pump_nano.state_cur = val;
+  if (pump_nano.state_old != pump_nano.state_cur) 
+  {
+    pump_nano.state_old = pump_nano.state_cur;
+    pump_nano.nextion_refresh = 1;
+    if (pump_nano.state_cur == 1) { digitalWrite(PUMP_NANO_GPIO, HIGH); }
+    else { digitalWrite(PUMP_NANO_GPIO, LOW); }
   }
 }
 
@@ -182,15 +250,9 @@ void oxygen_concentrator_state_update(uint8_t val)
   if (oxygen_concentrator.state_old != oxygen_concentrator.state_cur) 
   {
     oxygen_concentrator.state_old = oxygen_concentrator.state_cur;
-    if (oxygen_concentrator.state_cur == 1) 
-    {
-      digitalWrite(OXYGEN_CONCENTRATOR_GPIO, HIGH);
-    }
-    else
-    {
-      digitalWrite(OXYGEN_CONCENTRATOR_GPIO, LOW);
-    }
     oxygen_concentrator.nextion_refresh = 1;
+    if (oxygen_concentrator.state_cur == 1) { digitalWrite(OXYGEN_CONCENTRATOR_GPIO, HIGH); }
+    else { digitalWrite(OXYGEN_CONCENTRATOR_GPIO, LOW); }
   }
 }
 void ozone_generator_state_update(uint8_t val)
@@ -199,98 +261,113 @@ void ozone_generator_state_update(uint8_t val)
   if (ozone_generator.state_old != ozone_generator.state_cur) 
   {
     ozone_generator.state_old = ozone_generator.state_cur;
-    if (ozone_generator.state_cur == 1) 
-    {
-      digitalWrite(OZONE_GENERATOR_GPIO, HIGH);
-    }
-    else
-    {
-      digitalWrite(OZONE_GENERATOR_GPIO, LOW);
-    }
     ozone_generator.nextion_refresh = 1;
+    if (ozone_generator.state_cur == 1) { digitalWrite(OZONE_GENERATOR_GPIO, HIGH); }
+    else { digitalWrite(OZONE_GENERATOR_GPIO, LOW); }
   }
 }
 
-void cycle_starting()
+void solenoid_state_update(uint8_t val)
 {
-  if (cycle_state == 0)
+  solenoid.state_cur = val;
+  if (solenoid.state_old != solenoid.state_cur) 
   {
-    digitalWrite(PUMP_VALVE_GPIO, HIGH);
+    solenoid.state_old = solenoid.state_cur;
+    solenoid.nextion_refresh = 1;
+    if (solenoid.state_cur == 1) { digitalWrite(SOLENOID_GPIO, HIGH); }
+    else { digitalWrite(SOLENOID_GPIO, LOW); }
+  }
+}
+
+void cycle_update()
+{
+  if (cycle.pressure_switch_state_old != cycle.pressure_switch_state_cur || cycle.flow_switch_state_old != cycle.flow_switch_state_cur )
+  {
+    cycle.pressure_switch_state_old = cycle.pressure_switch_state_cur;
+    cycle.flow_switch_state_old = cycle.flow_switch_state_cur;
+    if (pressure_switch.state_cur == 1)
+    {
+      valve_input_state_update(0);
+      valve_bypass_1_state_update(0);
+      valve_bypass_2_state_update(1);
+      oxygen_concentrator_state_update(0);
+      ozone_generator_state_update(0);
+      solenoid_state_update(0);
+      cycle.state_cur = 0;
+    }
+    else if (pressure_switch.state_cur == 0)
+    {
+      if (flow_switch.state_cur == 1)
+      {
+        valve_input_state_update(1);
+        valve_bypass_1_state_update(1);
+        valve_bypass_2_state_update(0);
+        cycle.state_cur = 100;
+      }
+      if (flow_switch.state_cur == 0)
+      {
+        valve_input_state_update(1);
+        valve_bypass_1_state_update(1);
+        valve_bypass_2_state_update(0);
+        oxygen_concentrator_state_update(0);
+        ozone_generator_state_update(0);
+        solenoid_state_update(0);
+        cycle.state_cur = 0;
+      }
+    }
+    cycle_millis = millis();
+  }
+
+  if (cycle.state_cur == 0)
+  {
+    valve_input_state_update(1);
     pump_booster_state_update(1);
-    
     if (millis() - cycle_millis > 1000)
     {
       cycle_millis = millis();
-      cycle_state = 1;
+      cycle.state_cur = 1;
     }
   }
-  else if (cycle_state == 1)
+  else if (cycle.state_cur == 1)
   {
-    digitalWrite(PUMP_NANO_GPIO, HIGH);
-    pump_nano.state_cur = 0;
-    if (pump_nano.state_old != pump_nano.state_cur) 
-    {
-      pump_nano.state_old = pump_nano.state_cur;
-      pump_nano.nextion_refresh = 1;
-    }
-
+    pump_nano_state_update(1);
     if (millis() - cycle_millis > 19000)
     {
       cycle_millis = millis();
-      cycle_state = 2;
+      cycle.state_cur = 2;
     }
   }
-
-  else if (cycle_state == 100)
+  else if (cycle.state_cur == 100)
   {
-    digitalWrite(PUMP_VALVE_GPIO, HIGH);
+    valve_input_state_update(1);
     pump_booster_state_update(1);
-    
     if (millis() - cycle_millis > 1000)
     {
       cycle_millis = millis();
-      cycle_state = 101;
+      cycle.state_cur = 101;
     }
   }
-  else if (cycle_state == 101)
+  else if (cycle.state_cur == 101)
   {
-    digitalWrite(PUMP_NANO_GPIO, HIGH);
-    pump_nano.state_cur = 0;
-    if (pump_nano.state_old != pump_nano.state_cur) 
-    {
-      pump_nano.state_old = pump_nano.state_cur;
-      pump_nano.nextion_refresh = 1;
-    }
+    pump_nano_state_update(1);
     if (millis() - cycle_millis > 1000)
     {
       cycle_millis = millis();
-      cycle_state = 102;
+      cycle.state_cur = 102;
     }
   }
-  else if (cycle_state == 102)
+  else if (cycle.state_cur == 102)
   {
     oxygen_concentrator_state_update(1);
     ozone_generator_state_update(1);
+    solenoid_state_update(1);
   }
   else
   {
-    digitalWrite(PUMP_VALVE_GPIO, LOW);
-    
     pump_booster_state_update(0);
-    digitalWrite(PUMP_NANO_GPIO, LOW);
-    pump_nano.state_cur = 1;
-    if (pump_nano.state_old != pump_nano.state_cur) 
-    {
-      pump_nano.state_old = pump_nano.state_cur;
-      pump_nano.nextion_refresh = 1;
-    }
+    pump_nano_state_update(0);
   }
 
-}
-
-void cycle_manager()
-{
-  cycle_starting();  
 }
 
 void setup() 
@@ -300,8 +377,8 @@ void setup()
 
   pinMode(FLOW_SWITCH_GPIO, INPUT_PULLUP);
   pinMode(PRESSURE_SWITCH_GPIO, INPUT_PULLUP);
-  pinMode(PUMP_VALVE_GPIO, OUTPUT);
-  digitalWrite(PUMP_VALVE_GPIO, LOW);
+  pinMode(VALVE_INPUT_GPIO, OUTPUT);
+  digitalWrite(VALVE_INPUT_GPIO, LOW);
   pinMode(PUMP_BOOSTER_GPIO, OUTPUT);
   digitalWrite(PUMP_BOOSTER_GPIO, LOW);
   pinMode(PUMP_NANO_GPIO, OUTPUT);
@@ -309,12 +386,14 @@ void setup()
   pinMode(VALVE_BYPASS_1_GPIO, OUTPUT);
   digitalWrite(VALVE_BYPASS_1_GPIO, LOW);
   pinMode(VALVE_BYPASS_2_GPIO, OUTPUT);
-  digitalWrite(VALVE_BYPASS_1_GPIO, LOW);
+  digitalWrite(VALVE_BYPASS_2_GPIO, LOW);
 
   pinMode(OXYGEN_CONCENTRATOR_GPIO, OUTPUT);
   digitalWrite(OXYGEN_CONCENTRATOR_GPIO, LOW);
   pinMode(OZONE_GENERATOR_GPIO, OUTPUT);
   digitalWrite(OZONE_GENERATOR_GPIO, LOW);
+  pinMode(SOLENOID_GPIO, OUTPUT);
+  digitalWrite(SOLENOID_GPIO, LOW);
   
   oxygen_sensor_serial.begin(9600, SERIAL_8N1, OXYGEN_SENSOR_RX_GPIO, OXYGEN_SENSOR_TX_GPIO);  // RO, DI
   pinMode(OXYGEN_SENSOR_RE_DE_GPIO, OUTPUT);
@@ -326,15 +405,18 @@ void setup()
 
 void loop() 
 {
+  flow_switch_state_update();
+  pressure_switch_state_update();
   oxygen_sensor_manager();
 
-  cycle_manager();
+  cycle_update();
 
   if (nextion.started == 0)
   {
-    if (millis() - nextion.starting_delay_millis > 2000)
+    if (millis() - nextion.starting_delay_millis > 3000)
     {
       nextion.started = 1;
+      nextion.page_cur = 1;
     }
   }
   if (nextion.started == 1)
@@ -347,60 +429,4 @@ void loop()
   // Serial.print("PRESSURE SWITCH: ");
   // Serial.println(digitalRead(PRESSURE_SWITCH_GPIO));
 
-  flow_switch.state_cur = digitalRead(FLOW_SWITCH_GPIO);
-  if (flow_switch.state_old != flow_switch.state_cur) 
-  {
-    flow_switch.state_old = flow_switch.state_cur;
-    flow_switch.nextion_refresh = 1;
-  }
-
-  pressure_switch_state_update();
-  // pressure_switch.state_cur = digitalRead(PRESSURE_SWITCH_GPIO);
-  // if (pressure_switch.state_old != pressure_switch.state_cur) 
-  // {
-  //   cycle_millis = millis();
-  //   pressure_switch.state_old = pressure_switch.state_cur;
-  //   pressure_switch.nextion_refresh = 1;
-  //   if (pressure_switch.state_cur == 1)
-  //   {
-  //     cycle_state = 100;
-  //     valve_bypass_1.state_cur = 1;
-  //     if (valve_bypass_1.state_old != valve_bypass_1.state_cur) 
-  //     {
-  //       valve_bypass_1.state_old = valve_bypass_1.state_cur;
-  //       valve_bypass_1.nextion_refresh = 1;
-  //       digitalWrite(VALVE_BYPASS_1_GPIO, HIGH);
-  //     }
-  //     valve_bypass_2.state_cur = 0;
-  //     if (valve_bypass_2.state_old != valve_bypass_2.state_cur) 
-  //     {
-  //       valve_bypass_2.state_old = valve_bypass_2.state_cur;
-  //       valve_bypass_2.nextion_refresh = 1;
-  //       digitalWrite(VALVE_BYPASS_2_GPIO, LOW);
-  //     }
-  //   }
-  //   else
-  //   {
-  //     cycle_state = 1;
-  //     valve_bypass_1.state_cur = 0;
-  //     if (valve_bypass_1.state_old != valve_bypass_1.state_cur) 
-  //     {
-  //       valve_bypass_1.state_old = valve_bypass_1.state_cur;
-  //       valve_bypass_1.nextion_refresh = 1;
-  //       digitalWrite(VALVE_BYPASS_1_GPIO, LOW);
-  //     }
-  //     valve_bypass_2.state_cur = 1;
-  //     if (valve_bypass_2.state_old != valve_bypass_2.state_cur) 
-  //     {
-  //       valve_bypass_2.state_old = valve_bypass_2.state_cur;
-  //       valve_bypass_2.nextion_refresh = 1;
-  //       digitalWrite(VALVE_BYPASS_2_GPIO, HIGH);
-  //     }
-  //     oxygen_concentrator_state_update(0);
-  //     ozone_generator_state_update(0);
-  //   }
-  // }
-  // else
-  // { 
-  // }
 }
--- products/ozonanopro/arduino/__main__/nextion.ino
+++ products/ozonanopro/arduino/__main__/nextion.ino
@@ -1,3 +1,4 @@
+uint8_t valve_input_id[] = {0x30};
 uint8_t flow_switch_id[] = {0x31};
 uint8_t pressure_switch_id[] = {0x31, 0x32};
 uint8_t valve_bypass_1_id[] = {0x32};
@@ -6,33 +7,100 @@ uint8_t pump_booster_id[] = {0x35};
 uint8_t pump_nano_id[] = {0x36};
 uint8_t oxygen_concentrator_id[] = {0x37};
 uint8_t ozone_generator_id[] = {0x38};
+uint8_t solenoid_id[] = {0x39};
 
-void nextion_update() 
+void nextion_update_page_home_valve_input() 
 {
-  uint8_t force_refresh = 0;
-  if (nextion.page_old != nextion.page_cur) 
-  {
-    nextion.page_old = nextion.page_cur;
-    force_refresh = 1;
-  }
-  if (nextion.page_cur == 1)  nextion_update_page_home(force_refresh);
-}
-
-void nextion_update_page_home_flow_switch() 
-{
-  if (pressure_switch.state_cur == 1) 
+  if (valve_input.state_cur == 1) 
   { 
-    uint8_t _buffer_color[] = { 0x74, flow_switch_id[0], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x33, 0x38, 0x35, 0xff, 0xff, 0xff };
+    uint8_t _buffer_color[] = { 0x74, valve_input_id[0], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x33, 0x38, 0x35, 0xff, 0xff, 0xff };
     for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
-    uint8_t _buffer_value[] = { 0x74, flow_switch_id[0], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x4E, 0x22, 0xff, 0xff, 0xff };
+    uint8_t _buffer_value[] = { 0x74, valve_input_id[0], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x4E, 0x22, 0xff, 0xff, 0xff };
     for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
   }
   else 
   {
-    uint8_t _buffer_color[] = { 0x74, flow_switch_id[0], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x35, 0x35, 0x38, 0x38, 0xff, 0xff, 0xff };
+    uint8_t _buffer_color[] = { 0x74, valve_input_id[0], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x35, 0x35, 0x38, 0x38, 0xff, 0xff, 0xff };
     for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
-    uint8_t _buffer_value[] = { 0x74, flow_switch_id[0], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x46, 0x46, 0x22, 0xff, 0xff, 0xff };
+    uint8_t _buffer_value[] = { 0x74, valve_input_id[0], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x46, 0x46, 0x22, 0xff, 0xff, 0xff };
     for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
+  }
+}
+
+void nextion_update_page_home_valve_bypass_1() 
+{
+  if (valve_bypass_1.state_cur == 1) 
+  { 
+    uint8_t _buffer_color[] = { 0x74, valve_bypass_1_id[0], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x33, 0x38, 0x35, 0xff, 0xff, 0xff };
+    for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
+    uint8_t _buffer_value[] = { 0x74, valve_bypass_1_id[0], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x4E, 0x22, 0xff, 0xff, 0xff };
+    for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
+  }
+  else 
+  {
+    uint8_t _buffer_color[] = { 0x74, valve_bypass_1_id[0], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x35, 0x35, 0x38, 0x38, 0xff, 0xff, 0xff };
+    for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
+    uint8_t _buffer_value[] = { 0x74, valve_bypass_1_id[0], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x46, 0x46, 0x22, 0xff, 0xff, 0xff };
+    for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
+  }
+}
+
+void nextion_update_page_home_valve_bypass_2() 
+{
+  if (valve_bypass_2.state_cur == 1) 
+  { 
+    uint8_t _buffer_color[] = { 0x74, valve_bypass_2_id[0], valve_bypass_2_id[1], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x33, 0x38, 0x35, 0xff, 0xff, 0xff };
+    for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
+    uint8_t _buffer_value[] = { 0x74, valve_bypass_2_id[0], valve_bypass_2_id[1], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x4E, 0x22, 0xff, 0xff, 0xff };
+    for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
+  }
+  else 
+  {
+    uint8_t _buffer_color[] = { 0x74, valve_bypass_2_id[0], valve_bypass_2_id[1], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x35, 0x35, 0x38, 0x38, 0xff, 0xff, 0xff };
+    for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
+    uint8_t _buffer_value[] = { 0x74, valve_bypass_2_id[0], valve_bypass_2_id[1], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x46, 0x46, 0x22, 0xff, 0xff, 0xff };
+    for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
+  }
+}
+
+void nextion_update_page_home_solenoid() 
+{
+  if (solenoid.state_cur == 1) 
+  { 
+    uint8_t _buffer_color[] = { 0x74, solenoid_id[0], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x33, 0x38, 0x35, 0xff, 0xff, 0xff };
+    for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
+    uint8_t _buffer_value[] = { 0x74, solenoid_id[0], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x4E, 0x22, 0xff, 0xff, 0xff };
+    for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
+  }
+  else 
+  {
+    uint8_t _buffer_color[] = { 0x74, solenoid_id[0], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x35, 0x35, 0x38, 0x38, 0xff, 0xff, 0xff };
+    for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
+    uint8_t _buffer_value[] = { 0x74, solenoid_id[0], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x46, 0x46, 0x22, 0xff, 0xff, 0xff };
+    for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
+  }
+}
+
+void nextion_update_page_home_flow_switch(uint8_t force_refresh) 
+{
+  
+  if (force_refresh || flow_switch.nextion_refresh == 1) 
+  {
+    flow_switch.nextion_refresh = 0;
+    if (flow_switch.state_cur == 1) 
+    { 
+      uint8_t _buffer_color[] = { 0x74, flow_switch_id[0], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x33, 0x38, 0x35, 0xff, 0xff, 0xff };
+      for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
+      uint8_t _buffer_value[] = { 0x74, flow_switch_id[0], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x4E, 0x22, 0xff, 0xff, 0xff };
+      for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
+    }
+    else 
+    {
+      uint8_t _buffer_color[] = { 0x74, flow_switch_id[0], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x35, 0x35, 0x38, 0x38, 0xff, 0xff, 0xff };
+      for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
+      uint8_t _buffer_value[] = { 0x74, flow_switch_id[0], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x46, 0x46, 0x22, 0xff, 0xff, 0xff };
+      for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
+    }
   }
 }
 
@@ -52,36 +120,6 @@ void nextion_update_page_home_pressure_switch()
     uint8_t _buffer_value[] = { 0x74, pressure_switch_id[0], pressure_switch_id[1], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x46, 0x46, 0x22, 0xff, 0xff, 0xff };
     for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
   }
-}
-
-void nextion_update_page_home_valve_bypass_1_on() 
-{
-  uint8_t _buffer_color[] = { 0x74, valve_bypass_1_id[0], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x33, 0x38, 0x35, 0xff, 0xff, 0xff };
-  for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
-  uint8_t _buffer_value[] = { 0x74, valve_bypass_1_id[0], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x4E, 0x22, 0xff, 0xff, 0xff };
-  for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
-}
-void nextion_update_page_home_valve_bypass_1_off() 
-{
-  uint8_t _buffer_color[] = { 0x74, valve_bypass_1_id[0], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x35, 0x35, 0x38, 0x38, 0xff, 0xff, 0xff };
-  for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
-  uint8_t _buffer_value[] = { 0x74, valve_bypass_1_id[0], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x46, 0x46, 0x22, 0xff, 0xff, 0xff };
-  for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
-}
-
-void nextion_update_page_home_valve_bypass_2_on() 
-{
-  uint8_t _buffer_color[] = { 0x74, valve_bypass_2_id[0], valve_bypass_2_id[1], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x33, 0x38, 0x35, 0xff, 0xff, 0xff };
-  for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
-  uint8_t _buffer_value[] = { 0x74, valve_bypass_2_id[0], valve_bypass_2_id[1], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x4E, 0x22, 0xff, 0xff, 0xff };
-  for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
-}
-void nextion_update_page_home_valve_bypass_2_off() 
-{
-  uint8_t _buffer_color[] = { 0x74, valve_bypass_2_id[0], valve_bypass_2_id[1], 0x2E, 0x70, 0x63, 0x6F, 0x3D, 0x35, 0x35, 0x35, 0x38, 0x38, 0xff, 0xff, 0xff };
-  for (uint8_t i = 0; i < sizeof(_buffer_color) / sizeof(uint8_t); i++) { Serial2.write(_buffer_color[i]); }
-  uint8_t _buffer_value[] = { 0x74, valve_bypass_2_id[0], valve_bypass_2_id[1], 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x4F, 0x46, 0x46, 0x22, 0xff, 0xff, 0xff };
-  for (uint8_t i = 0; i < sizeof(_buffer_value) / sizeof(uint8_t); i++) { Serial2.write(_buffer_value[i]); }
 }
 
 void nextion_update_page_home_pump_booster_on() 
@@ -147,7 +185,13 @@ void nextion_update_page_home_ozone_generator_off()
 void nextion_update_page_home(uint8_t force_refresh) 
 {
   if (force_refresh) 
-  {}
+  {
+    uint8_t _buffer[] = { 0x70, 0x61, 0x67, 0x65, 0x20, 0x70, 0x5F, 0x68, 0x6F, 0x6D, 0x65, 0xff, 0xff, 0xff };  
+    for (uint8_t i = 0; i < sizeof(_buffer) / sizeof(uint8_t); i++) 
+    {
+      Serial2.write(_buffer[i]);
+    }
+  }
 
   if (force_refresh || oxygen_sensor.concentration_old != oxygen_sensor.concentration_cur) 
   {
@@ -191,30 +235,30 @@ void nextion_update_page_home(uint8_t force_refresh)
     }
   }
 
-  if (force_refresh || flow_switch.nextion_refresh == 1) 
+  if (force_refresh || valve_input.nextion_refresh == 1) 
   {
-    flow_switch.nextion_refresh = 0;
-    nextion_update_page_home_flow_switch();
-  }
-
-  if (force_refresh || pressure_switch.nextion_refresh == 1) 
-  {
-    pressure_switch.nextion_refresh = 0;
-    nextion_update_page_home_pressure_switch();
+    valve_input.nextion_refresh = 0;
+    nextion_update_page_home_valve_input();
   }
 
   if (force_refresh || valve_bypass_1.nextion_refresh == 1) 
-  {
+  {    
     valve_bypass_1.nextion_refresh = 0;
-    if (valve_bypass_1.state_cur == 0) { nextion_update_page_home_valve_bypass_1_on(); }
-    else { nextion_update_page_home_valve_bypass_1_off(); }
+    nextion_update_page_home_valve_bypass_1();
   }
 
   if (force_refresh || valve_bypass_2.nextion_refresh == 1) 
   {
     valve_bypass_2.nextion_refresh = 0;
-    if (valve_bypass_2.state_cur == 0) { nextion_update_page_home_valve_bypass_2_on(); }
-    else { nextion_update_page_home_valve_bypass_2_off(); }
+    nextion_update_page_home_valve_bypass_2();
+  }
+
+  nextion_update_page_home_flow_switch(force_refresh);
+
+  if (force_refresh || pressure_switch.nextion_refresh == 1) 
+  {
+    pressure_switch.nextion_refresh = 0;
+    nextion_update_page_home_pressure_switch();
   }
 
   if (force_refresh || pump_booster.nextion_refresh == 1) 
@@ -227,7 +271,7 @@ void nextion_update_page_home(uint8_t force_refresh)
   if (force_refresh || pump_nano.nextion_refresh == 1) 
   {
     pump_nano.nextion_refresh = 0;
-    if (pump_nano.state_cur == 0) { nextion_update_page_home_pump_nano_on(); }
+    if (pump_nano.state_cur == 1) { nextion_update_page_home_pump_nano_on(); }
     else { nextion_update_page_home_pump_nano_off(); }
   }
 
@@ -244,4 +288,22 @@ void nextion_update_page_home(uint8_t force_refresh)
     if (ozone_generator.state_cur == 1) { nextion_update_page_home_ozone_generator_on(); }
     else { nextion_update_page_home_ozone_generator_off(); }
   }
+  
+  if (force_refresh || solenoid.nextion_refresh == 1) 
+  {
+    solenoid.nextion_refresh = 0;
+    nextion_update_page_home_solenoid();
+  }
+}
+
+
+void nextion_update() 
+{
+  uint8_t force_refresh = 0;
+  if (nextion.page_old != nextion.page_cur) 
+  {
+    nextion.page_old = nextion.page_cur;
+    force_refresh = 1;
+  }
+  if (nextion.page_cur == 1)  nextion_update_page_home(force_refresh);
 }
