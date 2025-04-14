305336c28c3f387afb2ec7eb4575f18f7fa1329c
Martin <martinpellizzer@gmail.com>
2025-04-14 09:58:44 +0200

    '

--- demos/flow-meter/yf-dn40/flowmeter.ino
+++ demos/flow-meter/yf-dn40/flowmeter.ino
@@ -1,23 +1,53 @@
 
-
-void frequency_calc(uint32_t n_pulses, uint32_t t_millis)
+void flowmeter_frequency_get()
 {
-  float t_millis_adj = (float)t_millis / (float)(millis() - previousMillis);
-  flowmeter.frequency_cur = t_millis_adj * n_pulses;
+  float t_millis_adj = (float)flowmeter.sampling_interval / (float)(millis() - flowmeter.sampling_millis);
+  flowmeter.frequency_cur = t_millis_adj * flowmeter.pulses_count_one_sec;
 }
 
-void flow_l_m_calc(float frequency)
+void flowmeter_flow_litre_minute_get()
 {
-  flowmeter.flow_l_m_cur = frequency / flowmeter.calibration_factor;
+  flowmeter.flow_l_m_cur = flowmeter.frequency_cur / flowmeter.calibration_factor;
 }
 
-void flow_ml_s_calc(float frequency)
+void flowmeter_flow_millilitre_second_get()
 {
-  flowmeter.flow_l_m_cur = frequency / flowmeter.calibration_factor;
+  flowmeter.flow_l_m_cur = flowmeter.frequency_cur / flowmeter.calibration_factor;
   flowmeter.flow_ml_s_cur = (flowmeter.flow_l_m_cur / 60) * 1000;
 }
 
-void flow_ml_tot_calc()
+void flowmeter_flow_millilitre_total_get()
 {
   flowmeter.flow_ml_tot += flowmeter.flow_ml_s_cur;
+}
+
+void flowmeter_flow_litre_minute_debug()
+{
+  Serial.print("Flow rate: ");
+  Serial.print(int(flowmeter.flow_l_m_cur));
+  Serial.print("L/min");
+  Serial.print("\t");
+}
+
+void flowmeter_flow_millilitre_tot_debug()
+{
+  Serial.print("Millilitre Total: ");
+  Serial.print(flowmeter.flow_ml_tot);
+  Serial.print("mL / ");
+}
+
+void flowmeter_flow_litre_tot_debug()
+{
+  Serial.print("Litre Total: ");
+  Serial.print(flowmeter.flow_ml_tot / 1000);
+  Serial.print("L");
+}
+
+
+void flowmeter_debug()
+{
+  flowmeter_flow_litre_minute_debug();
+  flowmeter_flow_millilitre_tot_debug();
+  flowmeter_flow_litre_tot_debug();
+  Serial.println();
 }
--- demos/flow-meter/yf-dn40/yf-dn40.ino
+++ demos/flow-meter/yf-dn40/yf-dn40.ino
@@ -3,6 +3,8 @@ typedef struct flowmeter_t
   const uint8_t gpio = 27;
   volatile uint16_t pulses_count = 0;
   uint16_t pulses_count_one_sec = 0;
+  uint32_t sampling_millis = 0;
+  uint32_t sampling_interval = 1000;
   float calibration_factor = 0.45;
   float frequency_cur = 0;
   float flow_l_m_cur = 0;
@@ -11,9 +13,22 @@ typedef struct flowmeter_t
 } flowmeter_t;
 flowmeter_t flowmeter;
 
-long currentMillis = 0;
-long previousMillis = 0;
-int interval = 1000;
+void flowmeter_manager()
+{
+  if (millis() - flowmeter.sampling_millis > flowmeter.sampling_interval) 
+  {
+    flowmeter.pulses_count_one_sec = flowmeter.pulses_count;
+    flowmeter.pulses_count = 0;
+
+    flowmeter_frequency_get();
+    flowmeter_flow_litre_minute_get();
+    flowmeter_flow_millilitre_second_get();
+    flowmeter_flow_millilitre_total_get();
+
+    flowmeter.sampling_millis = millis();
+    flowmeter_debug();
+  }
+}
 
 void IRAM_ATTR pulse_counter()
 {
@@ -23,40 +38,13 @@ void IRAM_ATTR pulse_counter()
 void setup()
 {
   Serial.begin(9600);
-
   pinMode(flowmeter.gpio, INPUT_PULLUP);
 
-  previousMillis = 0;
-
-  attachInterrupt(digitalPinToInterrupt(flowmeter.gpio), pulse_counter, FALLING);
+  flowmeter.sampling_millis = 0;
+  attachInterrupt(digitalPinToInterrupt(flowmeter.gpio), pulse_counter, RISING);
 }
 
 void loop()
 {
-  currentMillis = millis();
-  if (currentMillis - previousMillis > interval) 
-  {
-    flowmeter.pulses_count_one_sec = flowmeter.pulses_count;
-    flowmeter.pulses_count = 0;
-
-    frequency_calc(flowmeter.pulses_count_one_sec, interval);
-    flow_l_m_calc(flowmeter.frequency_cur);
-    flow_ml_s_calc(flowmeter.frequency_cur);
-    flow_ml_tot_calc();
-
-    previousMillis = millis();
-    
-    // Print the flow rate for this second in litres / minute
-    Serial.print("Flow rate: ");
-    Serial.print(int(flowmeter.flow_l_m_cur));  // Print the integer part of the variable
-    Serial.print("L/min");
-    Serial.print("\t");       // Print tab space
-
-    // Print the cumulative total of litres flowed since starting
-    Serial.print("Output Liquid Quantity: ");
-    Serial.print(flowmeter.flow_ml_tot);
-    Serial.print("mL / ");
-    Serial.print(flowmeter.flow_ml_tot / 1000);
-    Serial.println("L");
-  }
+  flowmeter_manager();
 }
