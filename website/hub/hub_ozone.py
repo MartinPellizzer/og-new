8bd925634656d78a082c2d90e165cec279b02159
Martin <martinpellizzer@gmail.com>
2025-10-30 12:26:17 +0100

    '

--- website/hub/hub_ozone.py
+++ website/hub/hub_ozone.py
@@ -200,7 +200,7 @@ def ozono_article_gen(article_url_slug):
             line = f'# {line}'
         lines.append(line)
     markdown_text = '\n'.join(lines)
-    html_article += markdown.markdown(markdown_text)
+    html_article += markdown.markdown(markdown_text, extensions=['tables'])
 
     html_article = html_article.replace('<p><img', '<p class="container-lg"><img class="article-img"')
     html_article = html_article.replace('<h1', '<h1 class="container-lg article-h1"')
@@ -252,9 +252,38 @@ def ozono_article_gen(article_url_slug):
     
 def ozono_gen():
     html_article = ''
-    html_heading = components.h2_default(
-        text= f'''Ozono''',
-    )
+    # html_article += f'''<div style="margin-bottom: 6rem"></div>'''
+    # html_article += blocks.heading_default_2(
+    #     title = components.h1_default(
+    #         text = f'''Ozono''',
+    #         align = f'center',
+    #     ),
+    #     paragraph = components.paragraph_default(
+    #         text = f'''Tutto quello che c'è da sapere sull'ozono.''',
+    #         align = f'center',
+    #     ),
+    # )
+    html_article += f'''
+        <div class="container-xl" style="margin-top: 5rem">
+            <h1 style="color: #222222; font-size: 3.0rem; line-height: 1; font-weight: normal; text-align: center; margin-bottom: 1rem;">Ozono</h1>
+            <p style="color: #555555; font-size: 1.25rem; line-height: 1; font-weight: normal; text-align: center;">Tutto quello che c'è da sapere sull'ozono.</p>
+        </div>
+    '''
+
+    html_heading = f'''
+        <div class="container-xl" style="margin-bottom: 3rem;">
+            <div style="display: flex; justify-content: space-between;">
+                <div>
+                    <h2 style="color: #222222; font-size: 2.25rem; line-height: 1; font-weight: normal; margin-bottom: 1rem;">Articoli principali</h1>
+                    <p style="color: #555555; font-size: 1.125rem; line-height: 1; font-weight: normal;">Scopri i principali argomenti di intersse sull'ozono.</p>
+                </div>
+                <div>
+                    <a style="display: inline-block; text-decoration: none; color: blue; margin-top: 0.5rem;" href="#">Visualizza tutti gli articoli</a>
+                </div>
+            </div>
+        </div>
+    '''
+
     image_h = '15rem'
     border_radius = '0.5rem'
     border_color = '#cdcdcd'
@@ -267,52 +296,69 @@ def ozono_gen():
             font-size: {g.typography_size_lg};
         "
     '''
+    data = [
+        {
+            'href': '''/ozono/chimica.html''',
+            'image_src': '/immagini/home/sanificazione-ozono.png',
+            'category': 'CHIMICA',
+            'title': '''Chimica dell'ozono: reazioni, meccanismi e implicazioni atmosferiche''',
+            'description': '''La molecola di ozono, O₃, presenta una struttura piegata con un angolo di circa 117°, dovuto all'ibridazione sp² degli atomi di ossigeno...''',
+            'date': '''6 OTTOBRE 2025''',
+            'datetime': '''2025-10-06''',
+        },
+        {
+            'href': '''/ozono/ambiente.html''',
+            'image_src': '/ozono.jpg',
+            'category': 'AMBIENTE',
+            'title': '''Ozono nell'ambiente: cos'è, dove si trova e quali sono i suoi effetti sull'atmosfera''',
+            'description': '''L'ozono è una delle molecole più affascinanti e controverse dell'atmosfera terrestre. Può essere allo stesso tempo un prezioso alleato contro i raggi ultravioletti e un pericoloso inquinante se presente nei bassi strati dell'aria.''',
+            'date': '''6 OTTOBRE 2025''',
+            'datetime': '''2025-10-06''',
+        },
+        {
+            'href': '''/ozono/sanificazione.html''',
+            'image_src': '/ozono-sanificazione.jpg',
+            'category': 'SANIFICAZIONE',
+            'title': '''Sanificazione a ozono: la soluzione naturale per igiene, sicurezza e benessere''',
+            'description': '''La sanificazione a ozono sta diventando una delle soluzioni più efficaci ed ecologiche per garantire la pulizia, la disinfezione e l'igiene di ambienti domestici, professionali e agricoli.''',
+            'date': '''6 OTTOBRE 2025''',
+            'datetime': '''2025-10-06''',
+        },
+        {
+            'href': '''/ozono/terapia.html''',
+            'image_src': '/ozono-terapia.jpg',
+            'category': 'OZONOTERAPIA',
+            'title': '''Ozonoterapia: applicazioni, meccanismi e prospettive della terapia con ozono''',
+            'description': '''Negli ultimi anni l’ozonoterapia ha guadagnato un ruolo crescente all’interno della medicina rigenerativa e funzionale.''',
+            'date': '''6 OTTOBRE 2025''',
+            'datetime': '''2025-10-06''',
+        },
+    ]
     html_cards = ''
-    html_cards += f'''
-        <a href="/ozono/chimica.html" style="text-decoration: none; color: #222222;">
-            <div style="background-color: {card_bg_color}; border: 1px solid {border_color}; border-radius: {border_radius};">
-                <img src='/immagini/home/sanificazione-ozono.png' style="height: {image_h}; object-fit: cover; border-top-left-radius: {border_radius}; border-top-right-radius: {border_radius};">
-                <div style="padding: 1rem 2rem;">
-                    <h3 {card_style_h3}>Chimica</h3>
-                    <p>Articolo sulla chimica dell'ozono.</p>
+    for item in data:
+        description = ''
+        for word in item['description'].split():
+            description += f'{word} '
+            if len(description) > 60:
+                break
+        description = description.strip() + '...'
+        html_cards += f'''
+            <a href="{item['href']}" style="text-decoration: none; color: #222222;">
+                <div style="background-color: {card_bg_color}; border: 1px solid {border_color}; border-radius: {border_radius};">
+                    <img src="{item['image_src']}" style="height: {image_h}; object-fit: cover; border-top-left-radius: {border_radius}; border-top-right-radius: {border_radius};">
+                    <div style="padding: 1rem 2rem;">
+                        <p style="color: #777777; font-size: 0.75rem; line-height: 1; font-weight: bold; letter-spacing: 1px; margin-bottom: 0.75rem;">{item['category']}</p>
+                        <h3 style="color: #333333; font-size: 1.5rem; line-height: 1.2; font-weight: normal; margin-bottom: 0.75rem;">{item['title']}</h3>
+                        <p style="color: #555555; font-size: 1rem; line-height: 1.375rem; font-weight: normal; margin-bottom: 5rem;">{description}</p>
+                        <div style="display: flex; justify-content: space-between; align-items: center;">
+                            <time datetime="{item['datetime']}" style="color: #777777; font-size: 0.75rem; line-height: 1; font-weight: bold; letter-spacing: 1px;">{item['date']}</time>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="blue"><path d="m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z"/></svg>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </a>
-    '''
-    html_cards += f'''
-        <a href="/ozono/ambiente.html" style="text-decoration: none; color: #222222;">
-            <div style="background-color: {card_bg_color}; border: 1px solid {border_color}; border-radius: {border_radius};">
-                <img src='/ozono.jpg' style="height: {image_h}; object-fit: cover; border-top-left-radius: {border_radius}; border-top-right-radius: {border_radius};">
-                <div style="padding: 1rem 2rem;">
-                    <h3 {card_style_h3}>Ambiente</h3>
-                    <p>Articolo sull'ozono ambientale.</p>
-                </div>
-            </div>
-        </a>
-    '''
-    html_cards += f'''
-        <a href="/ozono/sanificazione.html" style="text-decoration: none; color: #222222;">
-            <div style="background-color: {card_bg_color}; border: 1px solid {border_color}; border-radius: {border_radius};">
-                <img src='/ozono-sanificazione.jpg' style="height: {image_h}; object-fit: cover; border-top-left-radius: {border_radius}; border-top-right-radius: {border_radius};">
-                <div style="padding: 1rem 2rem;">
-                    <h3 {card_style_h3}>Sanificazione</h3>
-                    <p>Articolo sulla sanificazione ad ozono.</p>
-                </div>
-            </div>
-        </a>
-    '''
-    html_cards += f'''
-        <a href="/ozono/terapia.html" style="text-decoration: none; color: #222222;">
-            <div style="background-color: {card_bg_color}; border: 1px solid {border_color}; border-radius: {border_radius};">
-                <img src='/ozono-terapia.jpg' style="height: {image_h}; object-fit: cover; border-top-left-radius: {border_radius}; border-top-right-radius: {border_radius};">
-                <div style="padding: 1rem 2rem;">
-                    <h3 {card_style_h3}">Ozonoterapia</h3>
-                    <p>Articolo sulla ozonoterapia.</p>
-                </div>
-            </div>
-        </a>
-    '''
-    html_article = sections.grid_1_default(html_heading, html_cards, html_buttons='')
+            </a>
+        '''
+    html_article += sections.grid_1_default(html_heading, html_cards, html_buttons='')
     html_index = f'''
         <!DOCTYPE html>
         <html lang="en">
