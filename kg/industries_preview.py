3b99993d1b57e7b7f4d35b1828c6b14a288be7d2
Martin <martinpellizzer@gmail.com>
2026-05-05 12:24:36 +0200

    '

--- kg/industries_preview.py
+++ kg/industries_preview.py
@@ -1,9 +1,32 @@
+import os
 import json
 
 from lib import io
 
 DATABASE_FOLDERPATH = f'/home/ubuntu/vault/ozonogroup/database'
 DATABASE_FOLDERPATH = f'C:/vault/ozonogroup/database'
+PROJECT_FOLDERPATH = f'C:/vault/ozonogroup'
+
+def studies__read_one_by_one(studies_folderpath):
+    filepaths = sorted([f'{studies_folderpath}/{filename}' for filename in os.listdir(studies_folderpath)])
+    for filepath in filepaths:
+        json_data = io.json_read(filepath)
+        abstract_title = json_data['PubmedArticle'][0]['MedlineCitation']['Article']['ArticleTitle']
+        try: abstract_text = ' '.join(json_data['PubmedArticle'][0]['MedlineCitation']['Article']['Abstract']['AbstractText'])
+        except: pass
+        # print(json.dumps(json_data, indent=4))
+        print()
+        print()
+        print()
+        print(abstract_title)
+        print('---')
+        print(abstract_text)
+        print()
+        print()
+        print()
+        input('>> ')
+        # quit()
+
 
 def nace_groups_print():
     groups_data = io.json_read(f'{DATABASE_FOLDERPATH}/studies/industries-grouped.json')
@@ -18,4 +41,5 @@ def nace_groups_print():
                     group_lvl_2['industry_llm'],'',
                 )
     
-nace_groups_print()
+studies__read_one_by_one(f'{PROJECT_FOLDERPATH}/studies/pubmed/ozone-wine/json')
+# nace_groups_print()
